0746144cbe0dae2ffcd8a90f51cbf00896fb18da
Matheus Alvarenga <matheus.alvarenga@natahouse.com>
2023-04-05 13:17:31 -0300

    feat: add deploy to gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Testing surge with new text</h1>
+      <h1>Master branch on gh pages</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
