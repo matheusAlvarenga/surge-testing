6e7e621b7f05b8ba7f5c0999dd5f3ae723e1817d
Matheus Alvarenga <matheus.alvarenga@natahouse.com>
2023-04-05 13:27:27 -0300

    feat: change text

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Branch pr-1 on surge</h1>
+      <h1>Branch pr-2 on surge v1</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
